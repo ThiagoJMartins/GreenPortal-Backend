9a1091774e2dcf273e442ab55bb14bd99f4c13af
ThiagoJMartins <thiagodev.martins@gmail.com>
2024-01-07 01:40:07 -0300

    pls work

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const FavoriteModel = require("./models/Favorite");
-const UserModel = require("./models/User");
+const Favorite = require("./models/Favorite");
+const User = require("./models/User");
 
 const sequelize = new Sequelize(
   "postgresql://postgres:5AFBeBB5-e22c5eDCbf3-d2Fa3**a3g2@roundhouse.proxy.rlwy.net:14304/railway",
   { logging: false, native: false }
 );
 
-FavoriteModel(sequelize);
-UserModel(sequelize);
+Favorite(sequelize);
+User(sequelize);
 
 const { User, Favorite } = sequelize.models;
-User.belongsToMany(Favorite, { through: UserFavs });
-Favorite.belongsToMany(User, { through: UserFavs });
+User.belongsToMany(Favorite, { through: "user_favorite" });
+Favorite.belongsToMany(User, { through: "user_favorite" });
 
 module.exports = {
   User,
