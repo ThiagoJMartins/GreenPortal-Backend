6425d7ae0e07e5981ac552bf3078b42d62e29481
ThiagoJMartins <thiagodev.martins@gmail.com>
2024-01-07 01:42:14 -0300

    .

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const Favorite = require("./models/Favorite");
-const User = require("./models/User");
+const FavoriteModel = require("./models/Favorite");
+const UserModel = require("./models/User");
 
 const sequelize = new Sequelize(
   "postgresql://postgres:5AFBeBB5-e22c5eDCbf3-d2Fa3**a3g2@roundhouse.proxy.rlwy.net:14304/railway",
   { logging: false, native: false }
 );
 
-Favorite(sequelize);
-User(sequelize);
+FavoriteModel(sequelize);
+UserModel(sequelize);
 
 const { User, Favorite } = sequelize.models;
 User.belongsToMany(Favorite, { through: "user_favorite" });
