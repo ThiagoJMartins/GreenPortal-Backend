1ebf1f166d891858d7488f3d2cc8aa566713f447
Thiago Martins <133309154+ThiagoJMartins@users.noreply.github.com>
2024-01-07 00:53:52 -0300

    Update DB_connection.js

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -5,7 +5,7 @@ const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
 const sequelize = new Sequelize(
-	`postgresql://postgres:5AFBeBB5-e22c5eDCbf3-d2Fa3**a3g2@roundhouse.proxy.rlwy.net:14304/railway`,
+	"postgresql://postgres:5AFBeBB5-e22c5eDCbf3-d2Fa3**a3g2@roundhouse.proxy.rlwy.net:14304/railway",
 	{ logging: false, native: false }
 );
 
